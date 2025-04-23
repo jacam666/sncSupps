69c188d1beda9f829d9d2a5cd0bcd1f24cae1af2
ja cam <jacam666@example.com>
2025-04-23 15:23:59 +0100

    fixed deployment issue

--- src/app/components/productCard.tsx
+++ src/app/components/productCard.tsx
@@ -4,6 +4,117 @@ import { Card, Group, Image, Text } from '@mantine/core';
 import classes from './CarouselCard.module.css';
 
 
+type Product = {
+    id: number;
+    name: string;
+    price: number;
+    images: string[];
+    size: string;
+    flavour: string;
+};
+
+const ProteinProducts: Product[] = [
+    {
+        id: 1,
+        name: 'THE ONLY WHEY PROTEIN',
+        price: 39.99,
+        images: [
+            'sncImages/only-whey-final-300x300.png',
+            'sncImages/only-whey-alt-1.png',
+        ],
+        flavour: '',
+        size: '2.2 Kg',
+    },
+    {
+        id: 2,
+        name: 'THE ONLY WHEY PROTEIN',
+        price: 29.99,
+        images: [
+            'sncImages/only-whey-final-300x300.png',
+            'sncImages/only-whey-alt-2.png',
+        ],
+        flavour: '',
+        size: '908g',
+    },
+    {
+        id: 3,
+        name: 'AFTER TRAIN ADVANCED',
+        price: 39.99,
+        images: [
+            'sncImages/advanced-aftertrain-strawberrylime-300x300.png',
+            'sncImages/advanced-aftertrain-alt-1.png',
+        ],
+        flavour: 'Strawberry and Lime',
+        size: '2Kg',
+    },
+    {
+        id: 4,
+        name: 'AFTER TRAIN ADVANCED',
+        price: 39.99,
+        images: [
+            'sncImages/AFTER-TRAIN-ADVANCED-300x300.png',
+            'sncImages/aftertrain-advanced-alt-1.png',
+        ],
+        flavour: 'Blue Candy Flavour',
+        size: '2Kg',
+    },
+    {
+        id: 5,
+        name: 'AFTER TRAIN ADVANCED',
+        price: 39.99,
+        images: [
+            'sncImages/orange-mango-advanced-aftertrain-for-web-300x300.png',
+            'sncImages/orange-mango-advanced-alt-1.png',
+        ],
+        flavour: 'Orange Mango Flavour',
+        size: '2Kg',
+    },
+    {
+        id: 6,
+        name: 'AFTER TRAIN',
+        price: 24.99,
+        images: [
+            'sncImages/1After_Train_Fruit_Punch-1-300x300.png',
+            'sncImages/1After_Train_Fruit_Punch-alt.png',
+        ],
+        flavour: 'Fruit Punch',
+        size: '908g',
+    },
+    {
+        id: 7,
+        name: 'AFTER TRAIN',
+        price: 24.99,
+        images: [
+            'sncImages/2After_Train_Raspberry-1-300x300.png',
+            'sncImages/2After_Train_Raspberry-alt.png',
+        ],
+        flavour: 'Raspberry',
+        size: '908g',
+    },
+    {
+        id: 8,
+        name: 'AFTER TRAIN',
+        price: 24.99,
+        images: [
+            'sncImages/apple-aftertrain-300x300.png',
+            'sncImages/apple-aftertrain-alt.png',
+        ],
+        flavour: 'Apple',
+        size: '908g',
+    },
+    {
+        id: 9,
+        name: 'AFTER TRAIN',
+        price: 24.99,
+        images: [
+            'sncImages/3After_Train_Orange-1-300x300.png',
+            'sncImages/3After_Train_Orange-alt.png',
+        ],
+        flavour: 'Orange',
+        size: '908g',
+    },
+];
+
 export function CarouselCard() {
     // const slides = ProteinProducts.map((image) => (
     //     <Carousel.Slide key={image}>
